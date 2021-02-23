dd8c15e8644f26dba755d512eec3cfd7e7f5a62c
elina <e_petrova@fanshaweonline.ca>
2021-02-22 20:22:31 -0500

    sql

--- server.js
+++ server.js
@@ -3,22 +3,32 @@ const express = require('express');
 // create Express object
 const app = express();
 
-const things = require("./routes/things");
-
-//middleware to handleallincoming and convert it to actual objects 
-//app.use(express.json());
-app.use("/things", things);
-//use the things.js file to handle endpoints 
-//that starts with /things
-
 //define port
 const port = process.env.PORT || 5050;
 
-// app
-//     .route("/things/cars")
-//     .get((req, res) => { })
-//     .post((req, res) => { });
+// specify middleware as the callback function 
+// make the middleware functon inside app.use,
+// no outside like var myLooger = function
+//(req res next){... next()}
 
+// runs when reload the page because
+// reloading = request
+app.use((req, res, next) => {
+    console.log('incoming request');
+    console.log(process.env.PORT);
+    console.log(process.env.NAME);
+
+    //call next() to control to the next callback
+    //it is  gonna be the secon callback
+
+    //just go down to other code
+    next();
+})
+
+app.use("/api", require("./routes/api"))
+
+//listen for connections on this port
+// conncetion happens once
 app.listen(port, () => {
     console.log(`server is running on ${port}`);
 })
